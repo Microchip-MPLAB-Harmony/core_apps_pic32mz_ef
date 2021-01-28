4a5276573f9fba003770e6a463c659b44a8b41d2
Amit Raddi <amit.raddi@microchip.com>
2021-01-28 18:55:29 +0530

    [DRV_USART] Regenerated demo for driver_usart_sync_usart_echo_pic32mz_ef_sk_freertos
    MH3-50041
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/usart/sync/usart_echo/firmware/src/config/pic32mz_ef_sk_freertos/initialization.c
+++ apps/driver/usart/sync/usart_echo/firmware/src/config/pic32mz_ef_sk_freertos/initialization.c
@@ -129,7 +129,7 @@ const DRV_USART_PLIB_INTERFACE drvUsart0PlibAPI = {
     .read = (DRV_USART_PLIB_READ)UART2_Read,
     .readIsBusy = (DRV_USART_PLIB_READ_IS_BUSY)UART2_ReadIsBusy,
     .readCountGet = (DRV_USART_PLIB_READ_COUNT_GET)UART2_ReadCountGet,
-	.readAbort = (DRV_USART_PLIB_READ_ABORT)UART2_ReadAbort,
+    .readAbort = (DRV_USART_PLIB_READ_ABORT)UART2_ReadAbort,
     .writeCallbackRegister = (DRV_USART_PLIB_WRITE_CALLBACK_REG)UART2_WriteCallbackRegister,
     .write = (DRV_USART_PLIB_WRITE)UART2_Write,
     .writeIsBusy = (DRV_USART_PLIB_WRITE_IS_BUSY)UART2_WriteIsBusy,
@@ -165,6 +165,8 @@ const DRV_USART_INIT drvUsart0InitData =
     .remapStopBits = drvUsart0remapStopBits,
 
     .remapError = drvUsart0remapError,
+
+    .dataWidth = DRV_USART_DATA_8_BIT,
 };
 
 // </editor-fold>
@@ -213,6 +215,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
     /* Start out with interrupts disabled before configuring any modules */
     __builtin_disable_interrupts();
 
