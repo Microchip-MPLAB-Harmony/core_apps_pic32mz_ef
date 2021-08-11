154f5fb0576de79b19916303435210fa3afb1215
Amit Raddi <amit.raddi@microchip.com>
2021-08-11 08:23:48 +0530

    [DRV_USART] Regenerated demo for driver_usart_sync_usart_echo_pic32mz_ef_sk_freertos
    MH3-55875
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/usart/sync/usart_echo/firmware/src/config/pic32mz_ef_sk_freertos/initialization.c
+++ apps/driver/usart/sync/usart_echo/firmware/src/config/pic32mz_ef_sk_freertos/initialization.c
@@ -153,10 +153,6 @@ const DRV_USART_INIT drvUsart0InitData =
     /* USART Client Objects Pool */
     .clientObjPool = (uintptr_t)&drvUSART0ClientObjPool[0],
 
-    .dmaChannelTransmit = SYS_DMA_CHANNEL_NONE,
-
-    .dmaChannelReceive = SYS_DMA_CHANNEL_NONE,
-
 
     .remapDataWidth = drvUsart0remapDataWidth,
 
@@ -221,7 +217,6 @@ void SYS_Initialize ( void* data )
 
   
     CLK_Initialize();
-    
     /* Configure Prefetch, Wait States and ECC */
     PRECONbits.PREFEN = 3;
     PRECONbits.PFMWS = 3;
