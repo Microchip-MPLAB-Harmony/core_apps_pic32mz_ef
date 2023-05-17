8b2baf24cc230fca3f976ad758c0c7e4d51a10e9
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-17 19:22:17 +0530

    [RTOS] Regenerated demo for rtos_freertos_basic_freertos_pic32mz_ef_sk
    MH3-76236

--- apps/rtos/freertos/basic_freertos/firmware/src/config/pic32mz_ef_sk/interrupts.c
+++ apps/rtos/freertos/basic_freertos/firmware/src/config/pic32mz_ef_sk/interrupts.c
@@ -48,10 +48,10 @@
 // Section: Included Files
 // *****************************************************************************
 // *****************************************************************************
-
 #include "configuration.h"
 #include "interrupts.h"
 #include "definitions.h"
+
 
 
 // *****************************************************************************
@@ -61,14 +61,23 @@
 // *****************************************************************************
 
 
-void CHANGE_NOTICE_B_InterruptHandler( void );
-void UART2_FAULT_InterruptHandler( void );
-void UART2_RX_InterruptHandler( void );
-void UART2_TX_InterruptHandler( void );
-
-
-
 /* All the handlers are defined here.  Each will call its PLIB-specific function. */
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Interrupt Vector declarations
+// *****************************************************************************
+// *****************************************************************************
+void CHANGE_NOTICE_B_Handler (void);
+void UART2_FAULT_Handler (void);
+void UART2_RX_Handler (void);
+void UART2_TX_Handler (void);
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Interrupt Vector definitions
+// *****************************************************************************
+// *****************************************************************************
 
 
 void CHANGE_NOTICE_B_Handler (void)
