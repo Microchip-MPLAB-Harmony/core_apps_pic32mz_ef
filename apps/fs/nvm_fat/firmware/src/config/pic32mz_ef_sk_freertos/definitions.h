2aa01653f93dd72b2fd88fd12db49dffc10a4302
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-17 19:25:21 +0530

    [SYS_FS] Regenerated demo for fs_nvm_fat_pic32mz_ef_sk_freertos
    MH3-76228

--- apps/fs/nvm_fat/firmware/src/config/pic32mz_ef_sk_freertos/definitions.h
+++ apps/fs/nvm_fat/firmware/src/config/pic32mz_ef_sk_freertos/definitions.h
@@ -64,12 +64,12 @@
 #include "bsp/bsp.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include "peripheral/nvm/plib_nvm.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
 #include "system/time/sys_time.h"
+#include "peripheral/nvm/plib_nvm.h"
 #include "app.h"
 
 
@@ -81,6 +81,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "PIC32MZ2048EFH144"
+#define DEVICE_ARCH			 "MIPS"
+#define DEVICE_FAMILY		 "PIC32MZEF"
+#define DEVICE_SERIES		 "PIC32MZ"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 200000000
