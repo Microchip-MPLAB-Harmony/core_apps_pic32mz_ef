c1484799501e8cc5a88eb018a5b7752a19aded16
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-05-17 19:22:13 +0530

    [SYS_FS] Regenerated demo for fs_nvm_fat_pic32mz_ef_sk
    MH3-76227

--- apps/fs/nvm_fat/firmware/src/config/pic32mz_ef_sk/system/cache/sys_cache.c
+++ apps/fs/nvm_fat/firmware/src/config/pic32mz_ef_sk/system/cache/sys_cache.c
@@ -136,26 +136,29 @@ void SYS_CACHE_InvalidateDCache (void)
     }
 }
 
-void SYS_CACHE_InvalidateDCache_by_Addr (uint32_t *addr, int32_t size)
+void SYS_CACHE_InvalidateDCache_by_Addr (void *addr, int32_t size)
 {
     if (CACHE_CacheCoherencyGet() != CACHE_DISABLE) // If Data Cache is enabled
     {
-        DCACHE_INVALIDATE_BY_ADDR((uint32_t)addr, (size_t)size);
+        DCACHE_INVALIDATE_BY_ADDR((uint32_t)(uint8_t*)addr, (size_t)size);
     }
 }
 
-void SYS_CACHE_CleanDCache_by_Addr (uint32_t *addr, int32_t size)
+void SYS_CACHE_CleanDCache_by_Addr (void *addr, int32_t size)
 {
     if (CACHE_CacheCoherencyGet() != CACHE_DISABLE) // If Data Cache is enabled
     {
-        DCACHE_CLEAN_BY_ADDR((uint32_t)addr, (size_t)size);
+        DCACHE_CLEAN_BY_ADDR((uint32_t)(uint8_t*)addr, (size_t)size);
+    }
+}
+/* MISRA C-2012 Rule 5.1 deviated:1 Deviation record ID -  H3_MISRAC_2012_R_5_2_DR_1 */
+
+void SYS_CACHE_CleanInvalidateDCache_by_Addr (void *addr, int32_t size)
+{
+    if (CACHE_CacheCoherencyGet() != CACHE_DISABLE) // If Data Cache is enabled
+    {
+        DCACHE_CLEAN_INVALIDATE_BY_ADDR((uint32_t)(uint8_t*)addr, (size_t)size);
     }
 }
 
-void SYS_CACHE_CleanInvalidateDCache_by_Addr (uint32_t *addr, int32_t size)
-{
-    if (CACHE_CacheCoherencyGet() != CACHE_DISABLE) // If Data Cache is enabled
-    {
-        DCACHE_CLEAN_INVALIDATE_BY_ADDR((uint32_t)addr, (size_t)size);
-    }
-}
+/* MISRAC 2012 deviation block end */
